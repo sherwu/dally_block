ed25879c726770c56752da8f6aae5ac8fde97532
Sherwin Wu <thesherwinwu@gmail.com>
2014-07-31 08:54:47 -0700

    started styling the popup

--- js/block.js
+++ js/block.js
@@ -2,5 +2,5 @@ var browserURL = window.location.href;
 console.log("WOW I GOT THE " + browserURL);
 
 if (browserURL.indexOf("facebook.com") >= 0) {
-  window.location.replace("http://oneplusone.productions");
+  //window.location.replace("http://oneplusone.productions");
 }
