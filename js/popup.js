51211d380b742197f0c04e467b3a9bed7c23dc6d
Sherwin Wu <thesherwinwu@gmail.com>
2014-07-31 12:59:40 -0700

    initial popup front end done

--- js/popup.js
+++ js/popup.js
@@ -9,6 +9,42 @@ function isSentenceFilled(blockSentence) {
 }
 
 $(function() {
-  $("input.block-input").blur(function() {
+  $("input.block-input-minutes").blur(function() {
+    var input = $(this)
+    var pluralString = $("span.block-input-minutes-s");
+    if (input.val() === "1" || input.val() === "01") {
+      pluralString.hide();
+    } else {
+      pluralString.show();
+    }
+  });
+
+  $("input.block-input-hours").blur(function() {
+    var input = $(this)
+    var pluralString = $("span.block-input-hours-s");
+    if (input.val() === "1" || input.val() === "01") {
+      pluralString.hide();
+    } else {
+      pluralString.show();
+    }
+  });
+
+  $("div.activate-button").click(function() {
+    if (isSentenceFilled($("div.block-sentence"))) {
+      var website = $("input.block-input-website").val().trim();
+      var usageMinutes = $("input.block-input-minutes").val().trim();
+      var resetHours = $("input.block-input-hours").val().trim();
+      var storageObject = {"usageMinutes": usageMinutes,
+                           "resetHours": resetHours};
+
+
+      chrome.storage.sync.get("dally_block_storage", function(dally_object) {
+        dally_object[website] = storageObject;
+        chrome.storage.sync.set({"dally_block_storage": dally_object}, function() {console.log("saved!")});
+      });
+    } else {
+      chrome.storage.sync.get("dally_block_storage", function(dally_object) {console.log(dally_object);});
+      console.log("not filled out yet");
+    }
   });
 });
